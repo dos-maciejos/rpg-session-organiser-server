a9d96d224decd4b10b99f894f391ae8091402244
Macikolej <oiejnik152@gmail.com>
2022-01-10 17:02:47 +0100

    Provisoric MVP

--- server/index.js
+++ server/index.js
@@ -22,50 +22,48 @@ let dates = [];
 let propositions = [];
 
 app.post("/session", (req, res) => {
-  console.log(req.body);
-  const userId = uuid.v4();
+  const accessToken = uuid.v4();
   let session = {
-    access_token: userId,
-    is_gm: false,
+    access_token: accessToken,
+    is_gm: !!req.body.is_gm,
   }
   if (gm === undefined) {
-    if (req.query.is_gm) {
+    if (session.is_gm) {
       gm = {
-        username: req.query.username,
-        uuid: userId,
+        username: req.body.username,
+        access_token: accessToken,
       }
-      session.is_gm = true;
     }
     else {
       users.push({
-        username: req.query.username,
-        uuid: userId,
+        username: req.body.username,
+        access_token: accessToken,
       })
     }
   }
-  else if (gm && gm.username === req.query.username) {
-    session.is_gm = true;
-    gm.uuid = userId;
+  else if (gm.username === req.body.username) {
+    gm.access_token = accessToken;
   }
   else {
+    session.is_gm = false;
     users.push({
-      username: req.query.username,
-      uuid: userId,
+      username: req.body.username,
+      access_token: accessToken,
     })
   }
   res.json(session)
 });
 
 app.post("/date", (req, res) => {
-  if (req.query.access_token === gm.uuid) {
+  if (req.body.access_token === gm.access_token) {
     dates.push({
-      hour: req.query.hour,
-      day: req.query.day,
-      month: req.query.month,
-      year: req.query.year,
+      hour: req.body.hour,
+      day: req.body.day,
+      month: req.body.month,
+      year: req.body.year,
     });
   }
-  res.json(dates)
+  res.json({});
 });
 
 app.get("/dates", (_, res) => {
@@ -86,20 +84,17 @@ const validateProposition = (proposition) => {
 }
 
 app.post("/proposition", (req, res) => {
-  if (validateProposition(req.query)) {
+  if (validateProposition(req.body) ) {
     propositions.push({
-      hour: req.query.hour,
-      day: req.query.day,
-      month: req.query.month,
-      year: req.query.year,
+      hour: req.body.hour,
+      day: req.body.day,
+      month: req.body.month,
+      year: req.body.year,
     });
   }
-  res.json(propositions)
+  res.json({});
 });
 
 app.get("/propositions", (_, res) => {
   res.json(propositions);
 });
-
-
-// TODO CLASS/FUNCTION OF USER, ADD USER ID AND ACCESS TOKEN SEPERATELY, VALIDATE !!
